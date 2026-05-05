2936e071015c20cd023589cc4177411e5d08eb0a
tomolatoon <tomo2000tomo170@gmail.com>
2026-05-06 01:49:52 +0900

    chore: UnoCSS presetWind3 を presetWind4 に移行

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,7 @@
-import { defineConfig, presetAttributify, presetIcons, presetWind3 } from 'unocss';
+import { defineConfig, presetAttributify, presetIcons, presetWind4 } from 'unocss';
 
 export default defineConfig({
-  presets: [presetWind3(), presetAttributify(), presetIcons({ scale: 1.2 })],
+  presets: [presetWind4(), presetAttributify(), presetIcons({ scale: 1.2 })],
   theme: {
     colors: {
       cyan: '#5dd8f0',
